53e7591756472c8d3e15c346120aa6bfa6932244
Architector #4 <architector4@mail.tilde.team>
2024-03-17 16:34:28 +0300

    Anti_NFT_Spam_Bot - fix logging of dependency crate warnings

--- anti_nft_spam_bot/src/main.rs
+++ anti_nft_spam_bot/src/main.rs
@@ -2,7 +2,7 @@ use arch_bot_commons::*;
 
 fn main() {
     if std::env::var_os("RUST_LOG").is_none() {
-        std::env::set_var("RUST_LOG", "WARNING,anti_nft_spam_bot=debug");
+        std::env::set_var("RUST_LOG", "WARN,anti_nft_spam_bot=debug");
     }
     start_everything(anti_nft_spam_bot::entry());
 }
