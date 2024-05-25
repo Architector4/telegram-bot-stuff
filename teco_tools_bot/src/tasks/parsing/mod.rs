f2222f6884dc6352e78a1d4c2283746b9b263705
Architector #4 <architector4@mail.tilde.team>
2024-05-25 22:44:29 +0300

    Teco Tools Bot - refactor image/video resize parsing helper funcs

--- teco_tools_bot/src/tasks/parsing/mod.rs
+++ teco_tools_bot/src/tasks/parsing/mod.rs
@@ -266,80 +266,6 @@ impl Task {
                     unreachable!();
                 };
 
-                fn perc_calc(
-                    percentage: f32,
-                    starting_dimension: NonZeroU32,
-                ) -> Option<NonZeroU32> {
-                    let factor = percentage / 100.0;
-
-                    if !factor.is_normal() || factor <= 0.0 {
-                        return None;
-                    }
-
-                    let dim = (starting_dimension.get() as f32 * factor) as u32;
-
-                    dim.try_into().ok()
-                }
-                fn single_dimension_parser(
-                    data: &str,
-                    starting: impl Into<Option<NonZeroU32>>,
-                ) -> Option<NonZeroU32> {
-                    if let Some(starting) = starting.into() {
-                        // Check if it's a percentage.
-                        if let Some(percent) = data.find('%') {
-                            if let Ok(percentage) = data[0..percent].parse::<f32>() {
-                                return perc_calc(percentage, starting);
-                            }
-                        }
-                    }
-                    let woot: NonZeroU32 = data.parse().ok()?;
-                    Some(woot)
-                }
-
-                fn percentage_parser(
-                    data: &str,
-                    starting_dimensions: (NonZeroU32, NonZeroU32),
-                ) -> Option<(NonZeroU32, NonZeroU32, f32)> {
-                    let percent = data.find('%')?;
-
-                    let percentage: f32 = data[0..percent].parse().ok()?;
-                    let width = perc_calc(percentage, starting_dimensions.0)?;
-                    let height = perc_calc(percentage, starting_dimensions.1)?;
-
-                    Some((width, height, percentage))
-                }
-
-                fn width_height_parser(
-                    data: &str,
-                    starting_dimensions: (NonZeroU32, NonZeroU32),
-                ) -> Option<(NonZeroU32, NonZeroU32)> {
-                    let x = data.find('x')?;
-                    let w = &data[0..x];
-                    let h = &data[x + 1..];
-                    // It's width and height.
-                    // Try in pixels...
-                    if let Some(width) = single_dimension_parser(w, starting_dimensions.0) {
-                        if let Some(height) = single_dimension_parser(h, starting_dimensions.1) {
-                            return Some((width, height));
-                        }
-                    }
-                    let width = single_dimension_parser(w, starting_dimensions.0)?;
-                    let height = single_dimension_parser(h, starting_dimensions.1)?;
-
-                    Some((width, height))
-                }
-
-                fn dimensions_parser(
-                    data: &str,
-                    starting_dimensions: (NonZeroU32, NonZeroU32),
-                ) -> Option<(NonZeroU32, NonZeroU32, f32)> {
-                    if let Some(x) = width_height_parser(data, starting_dimensions) {
-                        Some((x.0, x.1, 0.0))
-                    } else {
-                        percentage_parser(data, starting_dimensions)
-                    }
-                }
-
                 let dimensions_parser_err =
                     |data| dimensions_parser(data, *old_dimensions).ok_or(());
 
@@ -352,127 +278,6 @@ impl Task {
                         Err(())
                     }
                 };
-
-                fn aspect_ratio_parser(
-                    (a, mut b): (&str, &str),
-                    starting_dimensions: (NonZeroU32, NonZeroU32),
-                ) -> Option<(NonZeroU32, NonZeroU32)> {
-                    let (width, height) = (
-                        starting_dimensions.0.get() as f64,
-                        starting_dimensions.1.get() as f64,
-                    );
-                    let ends_in_plus = if b.ends_with('+') {
-                        b = &b[0..b.len() - 1];
-                        true
-                    } else {
-                        false
-                    };
-
-                    let (a, b): (f64, f64) = (a.parse().ok()?, b.parse().ok()?);
-
-                    // We now have an aspect ratio. Figure out two resolutions.
-                    // A smaller one that will fit within the original image snugly,
-                    // and a bigger one that will fit the original image within itself snugly.
-
-                    let fit_by_width = (width, (width * b) / a);
-
-                    let fit_by_height = ((height * a) / b, height);
-
-                    // True if fit_by_width is the bigger one,
-                    // i.e. fits the original image within itself.
-                    let fit_by_width_is_bigger =
-                        fit_by_width.0 > fit_by_height.0 || fit_by_width.1 > fit_by_height.1;
-
-                    // If we don't have a plus, then we need the smaller one.
-                    // If we do have a plus, then we need the bigger one.
-                    // Perfect situation for a XOR lol
-                    let fit_by_height_needed = fit_by_width_is_bigger ^ ends_in_plus;
-
-                    let wanted = if fit_by_height_needed {
-                        fit_by_height
-                    } else {
-                        fit_by_width
-                    };
-
-                    let wanted = (wanted.0.round(), wanted.1.round());
-
-                    // Resulting dimensions may not fit as u32 or be nonsense. Fail if so.
-                    if !wanted.0.is_finite()
-                        || !wanted.1.is_finite()
-                        || wanted.0 <= 0.0
-                        || wanted.0 > u32::MAX.into()
-                        || wanted.1 <= 0.0
-                        || wanted.1 > u32::MAX.into()
-                    {
-                        return None;
-                    }
-
-                    // This is very unlikely to fail. It was already parsed as NonZeroU32 in the
-                    // first place, and the math shouldn't exceed the limits.
-                    // Still, maybe the user would specify insanely huge numbers for an aspect
-                    // ratio? lol
-                    Some((
-                        NonZeroU32::new(wanted.0 as u32)?,
-                        NonZeroU32::new(wanted.1 as u32)?,
-                    ))
-                }
-
-                /// Returns rotation in degrees, and a boolean denoting if there
-                /// was anyindication that this value is specifically a rotation.
-                fn rotation_parser(data: &str) -> Option<(f64, bool)> {
-                    let new_data_deg = data
-                        .trim_end_matches("deg")
-                        .trim_end_matches('d')
-                        .trim_end_matches('°');
-                    let new_data_rad = data
-                        .trim_end_matches("rad")
-                        .trim_end_matches('r')
-                        .trim_end_matches('㎭');
-
-                    let matched_degrees = new_data_deg.len() != data.len();
-                    let matched_radians = new_data_rad.len() != data.len();
-
-                    let mut matched_anything = matched_degrees || matched_radians;
-
-                    if matched_degrees && matched_radians {
-                        // Both matched. We got nonsense. Bye!
-                        return None;
-                    }
-
-                    // Default assumption is degrees.
-                    let in_radians = matched_radians;
-
-                    let data = if in_radians {
-                        new_data_rad
-                    } else {
-                        new_data_deg
-                    };
-
-                    // Pi/Tau checks because I know someone will try this lmao
-                    let mut rotation: f64 = if data == "π" || data == "Π" {
-                        matched_anything = true;
-                        if matched_radians {
-                            // Nonsense.
-                            return None;
-                        }
-                        std::f64::consts::PI.to_degrees()
-                    } else if data == "τ" || data == "Τ" {
-                        matched_anything = true;
-                        if matched_radians {
-                            // Nonsense.
-                            return None;
-                        }
-                        std::f64::consts::TAU.to_degrees()
-                    } else {
-                        data.parse().ok()?
-                    };
-
-                    if in_radians {
-                        rotation = rotation.to_degrees();
-                    }
-
-                    Some((rotation, matched_anything))
-                }
 
                 for param in params {
                     if !is_video {
@@ -622,5 +427,311 @@ impl Task {
                 }
             }
         }
+    }
+}
+
+///////////////////////
+////////// HELPER FUNCTIONS
+//////////////////////
+
+/// Given a `percentage` and a `input`, sanitize `percentage` and
+/// compute a value that is that much percentage of that input.
+fn perc_calc(percentage: f32, input: NonZeroU32) -> Option<NonZeroU32> {
+    let factor = percentage / 100.0;
+
+    if !factor.is_normal() || factor <= 0.0 {
+        return None;
+    }
+
+    let dim = (input.get() as f32 * factor) as u32;
+
+    dim.try_into().ok()
+}
+
+#[test]
+fn perc_calc_test() {
+    let input = NonZeroU32::new(144).unwrap();
+    assert_eq!(perc_calc(100.0, input), Some(input));
+    let result = NonZeroU32::new(72).unwrap();
+    assert_eq!(perc_calc(50.0, input), Some(result));
+    assert_eq!(perc_calc(f32::NAN, input), None);
+}
+
+/// Returns rotation in degrees, and a boolean denoting if there
+/// was any indication that this value is specifically a rotation.
+fn rotation_parser(data: &str) -> Option<(f64, bool)> {
+    let new_data_deg = data
+        .trim_end_matches("deg")
+        .trim_end_matches('d')
+        .trim_end_matches('°');
+    let new_data_rad = data
+        .trim_end_matches("rad")
+        .trim_end_matches('r')
+        .trim_end_matches('㎭');
+
+    let matched_degrees = new_data_deg.len() != data.len();
+    let matched_radians = new_data_rad.len() != data.len();
+
+    let mut matched_anything = matched_degrees || matched_radians;
+
+    if matched_degrees && matched_radians {
+        // Both matched. We got nonsense. Bye!
+        return None;
+    }
+
+    // Default assumption is degrees.
+    let in_radians = matched_radians;
+
+    let data = if in_radians {
+        new_data_rad
+    } else {
+        new_data_deg
+    };
+
+    // Pi/Tau checks because I know someone will try this lmao
+    let mut rotation: f64 = if data == "π" || data == "Π" {
+        matched_anything = true;
+        if matched_radians {
+            // Nonsense.
+            return None;
+        }
+        std::f64::consts::PI.to_degrees()
+    } else if data == "τ" || data == "Τ" {
+        matched_anything = true;
+        if matched_radians {
+            // Nonsense.
+            return None;
+        }
+        std::f64::consts::TAU.to_degrees()
+    } else {
+        data.parse().ok()?
+    };
+
+    if in_radians {
+        rotation = rotation.to_degrees();
+    }
+
+    Some((rotation, matched_anything))
+}
+
+#[test]
+fn rotation_parser_test() {
+    assert_eq!(rotation_parser("60"), Some((60.0, false)));
+    assert_eq!(rotation_parser("90deg"), Some((90.0, true)));
+    assert_eq!(rotation_parser("9999°"), Some((9999.0, true)));
+    assert_eq!(rotation_parser("90deg°rad"), None);
+    assert_eq!(rotation_parser("waoidjfa0w9tj3q0j"), None);
+}
+
+/// Parses input tuples as a specification of an aspect ratio
+/// either cropping or enclosing the starting dimensions, and
+/// outputs result.
+fn aspect_ratio_parser(
+    (a, mut b): (&str, &str),
+    starting_dimensions: (NonZeroU32, NonZeroU32),
+) -> Option<(NonZeroU32, NonZeroU32)> {
+    let (width, height) = (
+        starting_dimensions.0.get() as f64,
+        starting_dimensions.1.get() as f64,
+    );
+    let ends_in_plus = if b.ends_with('+') {
+        b = &b[0..b.len() - 1];
+        true
+    } else {
+        false
+    };
+
+    let (a, b): (f64, f64) = (a.parse().ok()?, b.parse().ok()?);
+
+    // We now have an aspect ratio. Figure out two resolutions.
+    // A smaller one that will fit within the original image snugly,
+    // and a bigger one that will fit the original image within itself snugly.
+
+    let fit_by_width = (width, (width * b) / a);
+
+    let fit_by_height = ((height * a) / b, height);
+
+    // True if fit_by_width is the bigger one,
+    // i.e. fits the original image within itself.
+    let fit_by_width_is_bigger =
+        fit_by_width.0 > fit_by_height.0 || fit_by_width.1 > fit_by_height.1;
+
+    // If we don't have a plus, then we need the smaller one.
+    // If we do have a plus, then we need the bigger one.
+    // Perfect situation for a XOR lol
+    let fit_by_height_needed = fit_by_width_is_bigger ^ ends_in_plus;
+
+    let wanted = if fit_by_height_needed {
+        fit_by_height
+    } else {
+        fit_by_width
+    };
+
+    let wanted = (wanted.0.round(), wanted.1.round());
+
+    // Resulting dimensions may not fit as u32 or be nonsense. Fail if so.
+    if !wanted.0.is_finite()
+        || !wanted.1.is_finite()
+        || wanted.0 <= 0.0
+        || wanted.0 > u32::MAX.into()
+        || wanted.1 <= 0.0
+        || wanted.1 > u32::MAX.into()
+    {
+        return None;
+    }
+
+    // This is very unlikely to fail. It was already parsed as NonZeroU32 in the
+    // first place, and the math shouldn't exceed the limits.
+    // Still, maybe the user would specify insanely huge numbers for an aspect
+    // ratio? lol
+    Some((
+        NonZeroU32::new(wanted.0 as u32)?,
+        NonZeroU32::new(wanted.1 as u32)?,
+    ))
+}
+
+#[test]
+fn aspect_ratio_parser_test() {
+    let start_dim = (NonZeroU32::new(100).unwrap(), NonZeroU32::new(150).unwrap());
+    let crop_1_1 = (NonZeroU32::new(100).unwrap(), NonZeroU32::new(100).unwrap());
+    let extend_1_1 = (NonZeroU32::new(150).unwrap(), NonZeroU32::new(150).unwrap());
+
+    assert_eq!(aspect_ratio_parser(("1", "1"), start_dim), Some(crop_1_1));
+    assert_eq!(
+        aspect_ratio_parser(("1", "1+"), start_dim),
+        Some(extend_1_1)
+    );
+    assert_eq!(aspect_ratio_parser(("2", "3"), start_dim), Some(start_dim));
+}
+
+fn single_dimension_parser(
+    data: &str,
+    starting: impl Into<Option<NonZeroU32>>,
+) -> Option<NonZeroU32> {
+    if let Some(starting) = starting.into() {
+        // Check if it's a percentage.
+        if let Some(percent) = data.find('%') {
+            // Check for garbage after the % sign.
+            if (data.len() - '%'.len_utf8()) != percent {
+                return None;
+            }
+
+            if let Ok(percentage) = data[0..percent].parse::<f32>() {
+                return perc_calc(percentage, starting);
+            }
+        }
+    }
+    let woot: NonZeroU32 = data.parse().ok()?;
+    Some(woot)
+}
+
+#[test]
+fn single_dimension_parser_test() {
+    let nzu32_60 = NonZeroU32::new(60).unwrap();
+    let nzu32_36 = NonZeroU32::new(36).unwrap();
+    assert_eq!(single_dimension_parser("60", None), Some(nzu32_60));
+    assert_eq!(single_dimension_parser("60%", None), None);
+    assert_eq!(
+        single_dimension_parser("60", Some(nzu32_60)),
+        Some(nzu32_60)
+    );
+    assert_eq!(
+        single_dimension_parser("60%", Some(nzu32_60)),
+        Some(nzu32_36)
+    );
+    assert_eq!(single_dimension_parser("60%wasd", Some(nzu32_60)), None);
+}
+
+/// Given a percentage in `data` and starting dimensions,
+/// parse and compute the percentage of those dimensions
+/// and return result.
+fn percentage_parser(
+    data: &str,
+    starting_dimensions: (NonZeroU32, NonZeroU32),
+) -> Option<(NonZeroU32, NonZeroU32, f32)> {
+    let percent = data.find('%')?;
+
+    // Check for garbage after the % sign.
+    if (data.len() - '%'.len_utf8()) != percent {
+        return None;
+    }
+
+    let percentage: f32 = data[0..percent].parse().ok()?;
+    let width = perc_calc(percentage, starting_dimensions.0)?;
+    let height = perc_calc(percentage, starting_dimensions.1)?;
+
+    Some((width, height, percentage))
+}
+
+#[test]
+fn percentage_parser_test() {
+    let start_dim = (NonZeroU32::new(100).unwrap(), NonZeroU32::new(150).unwrap());
+    let dim_150percent = (
+        NonZeroU32::new(150).unwrap(),
+        NonZeroU32::new(225).unwrap(),
+        150.0,
+    );
+    let dim_50percent = (
+        NonZeroU32::new(50).unwrap(),
+        NonZeroU32::new(75).unwrap(),
+        50.0,
+    );
+    assert_eq!(
+        percentage_parser("100%", start_dim),
+        Some((start_dim.0, start_dim.1, 100.0))
+    );
+    assert_eq!(percentage_parser("150%", start_dim), Some(dim_150percent));
+    assert_eq!(percentage_parser("50%", start_dim), Some(dim_50percent));
+    assert_eq!(percentage_parser("50%woidahjsod", start_dim), None);
+    assert_eq!(percentage_parser("6", start_dim), None);
+}
+
+/// Given a width and height specification, either in percentages of
+/// starting dimensions or absolute values, and those starting dimensions,
+/// parse, compute and return result.
+fn width_height_parser(
+    data: &str,
+    starting_dimensions: (NonZeroU32, NonZeroU32),
+) -> Option<(NonZeroU32, NonZeroU32)> {
+    let x = data.find('x')?;
+    let w = &data[0..x];
+    let h = &data[x + 1..];
+    // It's width and height.
+    // Try in pixels...
+    if let Some(width) = single_dimension_parser(w, starting_dimensions.0) {
+        if let Some(height) = single_dimension_parser(h, starting_dimensions.1) {
+            return Some((width, height));
+        }
+    }
+    let width = single_dimension_parser(w, starting_dimensions.0)?;
+    let height = single_dimension_parser(h, starting_dimensions.1)?;
+
+    Some((width, height))
+}
+#[test]
+fn width_height_parser_test() {
+    let start_dim = (NonZeroU32::new(100).unwrap(), NonZeroU32::new(150).unwrap());
+    let dim_150x150 = (NonZeroU32::new(150).unwrap(), NonZeroU32::new(150).unwrap());
+
+    // to make it shorter so that rustfmt doesn't split the asserts into many lines lol
+    let the_fn = width_height_parser;
+
+    assert_eq!(the_fn("150x150", start_dim), Some(dim_150x150));
+    assert_eq!(the_fn("150x100%", start_dim), Some(dim_150x150));
+    assert_eq!(the_fn("150%x100%", start_dim), Some(dim_150x150));
+    assert_eq!(the_fn("150x0%", start_dim), None);
+}
+
+/// Given either a percentage or width/height specification
+/// and starting dimensions, parse, compute, return output dimensions.
+/// Also computes a percentage value, if applicable, else returns 0.0 for it.
+fn dimensions_parser(
+    data: &str,
+    starting_dimensions: (NonZeroU32, NonZeroU32),
+) -> Option<(NonZeroU32, NonZeroU32, f32)> {
+    if let Some(x) = width_height_parser(data, starting_dimensions) {
+        Some((x.0, x.1, 0.0))
+    } else {
+        percentage_parser(data, starting_dimensions)
     }
 }
