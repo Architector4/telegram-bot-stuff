e717f1bd5afbc223fd3aee5b3cf2566bad634ed6
Architector #4 <architector4@mail.tilde.team>
2025-06-02 04:50:21 +0300

    Anti NFT Spam Bot - expand "could not find any links" message

--- anti_nft_spam_bot/src/handlers/mod.rs
+++ anti_nft_spam_bot/src/handlers/mod.rs
@@ -552,7 +552,11 @@ async fn gather_suspicion(
                 response.as_str()
             } else {
                 // No links at all.
-                "Sorry, but I could not find any links."
+                concat!(
+                    "Sorry, but I could not find any links in ",
+                    "your message or the message you replied to, if any. ",
+                    "This bot only blocks messages with usernames, links and buttons with links."
+                )
             };
             bot.archsendmsg(message.chat.id, response, message.id)
                 .await?;
