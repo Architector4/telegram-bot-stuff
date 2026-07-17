c4105d971691c7db39881f2829f22d44ae626f95
Architector #4 <architector4@mail.tilde.team>
2026-07-17 04:53:43 +0300

    use placebobuild instead of release profile

--- buildall.sh
+++ buildall.sh
@@ -8,9 +8,11 @@ for i in . */; do
 		chrt -i 0 ionice -c 3 cargo check-all-features -- --keep-going&
 		chrt -i 0 ionice -c 3 cargo clippy --all-targets --keep-going&
 		chrt -i 0 ionice -c 3 cargo build --keep-going&
-		chrt -i 0 ionice -c 3 cargo build --release --keep-going&
 		#chrt -i 0 ionice -c 3 cargo doc --keep-going& # ok no docs
 		chrt -i 0 ionice -c 3 cargo test&
+		# For context on "placebobuild", check out my cargo config:
+		# https://github.com/Architector4/dotfiles-n-stuff/blob/master/.cargo/config.toml
+		chrt -i 0 ionice -c 3 cargo placebobuild --keep-going&
 		wait
 		)&
 done
